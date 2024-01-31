f80e5d7fe5fe5a098bd0ad0f07d7ec68da001538
martyasbutkus <noobtuberis1990@gmail.com>
2024-01-31 17:37:47 +0200

    fix

--- src/Pages/Main/Footer.jsx
+++ src/Pages/Main/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="button effect">
-        <h1 style={{ paddingTop: '20px' }}>Kaišiadorių raj., Rumšiškių sen., Vosyliškių k. 1</h1>
+        <h1 style={{ paddingTop: '10px' }}>Kaišiadorių raj., Rumšiškių sen., Vosyliškių k. 1</h1>
         <h1>akacijuziedai20@gmail.com</h1>
         <h1>+370 674 94992, +370 699 11715</h1>
         <h1>www.akacijuziedai.lt</h1>
